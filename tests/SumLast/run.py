dfcfa0c384e1584586f8008f8b6d739bb140d26e
Marco Stoffel <marco.stoffel@softwareag.com>
2020-04-17 13:50:23 +0200

    Added SumLast Block. It outputs the sum of the value of all previous received events.

---
+++ tests/SumLast/run.py
@@ -0,0 +1,37 @@
+#
+#  $Copyright (c) 2019 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors.$
+#   This file is licensed under the Apache 2.0 license - see https://www.apache.org/licenses/LICENSE-2.0
+#
+from pysys.constants import *
+from apamax.analyticsbuilder.basetest import AnalyticsBuilderBaseTest
+
+class PySysTest(AnalyticsBuilderBaseTest):
+	def execute(self):
+		correlator = self.startAnalyticsBuilderCorrelator(blockSourceDir=f'{self.project.SOURCE}/blocks/')
+		modelId = self.createTestModel('apamax.analyticsbuilder.customblocks.SumLast')
+		self.sendEventStrings(
+			correlator,
+			self.timestamp(1),
+			self.inputEvent('value', 5.0,  id = modelId),
+			self.timestamp(2),
+			self.inputEvent('value', 10.0, id = modelId),
+			self.timestamp(3),
+			self.inputEvent('value', -10.0, id = modelId),
+			self.timestamp(4),
+			self.inputEvent('value', -10.0, id = modelId),
+			self.inputEvent('reset', True, id = modelId),
+			self.timestamp(5),
+			)
+	def validate(self):
+
+		# Verifying the result - output from the block.
+
+		self.assertGrep('output.evt', expr=self.outputExpr('sum', 5))
+		self.assertGrep('output.evt', expr=self.outputExpr('lastValue', 0))
+		self.assertGrep('output.evt', expr=self.outputExpr('sum', 15))
+		self.assertGrep('output.evt', expr=self.outputExpr('lastValue', 5))
+		self.assertGrep('output.evt', expr=self.outputExpr('sum', 5))
+		self.assertGrep('output.evt', expr=self.outputExpr('lastValue', 15))
+		self.assertGrep('output.evt', expr=self.outputExpr('sum', 0))
+		self.assertGrep('output.evt', expr=self.outputExpr('lastValue', 5))
+
